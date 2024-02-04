a06fa9036dd0406bb39f5e92bf0868d2e422d2e1
David <dvdcharles222@gmail.com>
2024-02-03 23:09:53 -0300

    Estudando js

--- projetos/4_ESTRUTURAS_DE_PROGRAMACAO/4_reservadas/scripts.js
+++ projetos/4_ESTRUTURAS_DE_PROGRAMACAO/4_reservadas/scripts.js
@@ -1,7 +1,21 @@
 // let if = 'Teste';
 // let function = 'teste';
-let functionTest = 'teste';
-let function1 = 'teste2';
+// let functionTest = 'teste';
+// let function1 = 'teste2';
 
-console.log(functionTest);
-console.log(function1);
+// console.log(functionTest);
+// console.log(function1);
+
+function tipo(num){
+    if(typeof num === 'number'){
+        console.log('Tipo Number '+ num)
+    }else if(typeof num === 'string'){
+        console.log('Tipo string '+ num)
+    }else if(typeof num === 'boolean'){
+        console.log('Tipo boolan ' + num)
+    }
+}
+tipo(5)
+tipo('David')
+tipo(true)
+

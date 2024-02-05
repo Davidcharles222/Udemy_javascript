b929208654cc891f8228f17f4f8ffb325d26dc46
David <dvdcharles222@gmail.com>
2024-02-04 23:00:46 -0300

    Aprendendo mais js

--- projetos/4_ESTRUTURAS_DE_PROGRAMACAO/4_reservadas/scripts.js
+++ projetos/4_ESTRUTURAS_DE_PROGRAMACAO/4_reservadas/scripts.js
@@ -5,17 +5,3 @@
 
 // console.log(functionTest);
 // console.log(function1);
-
-function tipo(num){
-    if(typeof num === 'number'){
-        console.log('Tipo Number '+ num)
-    }else if(typeof num === 'string'){
-        console.log('Tipo string '+ num)
-    }else if(typeof num === 'boolean'){
-        console.log('Tipo boolan ' + num)
-    }
-}
-tipo(5)
-tipo('David')
-tipo(true)
-

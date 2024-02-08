e8a8f0b3b7699258fb19479d8d25f28d18af4a62
David <dvdcharles222@gmail.com>
2024-02-07 23:02:07 -0300

    Aprendendo sobre expressoes regulares

--- projetos/11_EXERCICIOS/02_exercicio_37/scripts.js
+++ projetos/11_EXERCICIOS/02_exercicio_37/scripts.js
@@ -1,102 +1,76 @@
-// class Carrinho {
-//   constructor(itens, qtd, valorTotal) {
-//     this.itens = itens;
-//     this.qtd = qtd;
-//     this.valorTotal = valorTotal;
-//   }
-
-//   addItem(item) {
-
-//     let contador = 0;
-
-//     for(let itemCarrinho in this.itens) {
-//       if(this.itens[itemCarrinho].id == item.id) {
-//         this.itens[itemCarrinho].qtd += item.qtd;
-//         contador = 1;
-//       }
-//     }
-
-//     if(contador == 0) {
-//       this.itens.push(item);
-//     }
-
-//     this.qtd += item.qtd;
-//     this.valorTotal += item.preco * item.qtd;
-
-//   }
-
-//   removeItem(item) {
-
-//     for(let itemCarrinho in this.itens) {
-//       if(this.itens[itemCarrinho].id == item.id) {
-        
-//         let obj = this.itens[itemCarrinho];
-//         let index = this.itens.findIndex(function(obj) { return obj.id == item.id});
-
-//         this.qtd -= this.itens[itemCarrinho].qtd;
-//         this.valorTotal -= this.itens[itemCarrinho].preco * this.itens[itemCarrinho].qtd;
-
-//         this.itens.splice(index, 1);
-
-//       }
-//     }
-//   }
-
-// }
-
-// let carrinho = new Carrinho([
-//   {
-//     id: 1,
-//     nome: "Camisa",
-//     qtd: 1,
-//     preco: 20
-//   },
-//   {
-//     id: 2,
-//     nome: "Calça",
-//     qtd: 2,
-//     preco: 50
-//   }
-// ], 3, 120);
-
-// console.log(carrinho);
-
-// carrinho.addItem({id:1, nome:"Camisa", qtd:2, preco: 20});
-
-// console.log(carrinho);
-
-// carrinho.addItem({id:3, nome:"Boné", qtd:1, preco: 15});
-
-// console.log(carrinho);
-
-// carrinho.removeItem({id: 1, nome: "Camisa", qtd: 1, preco: 20});
-
-// console.log(carrinho);
-
-
-class Voo{
-  constructor(codVoo,origem,destino,assentosDisponiveis){
-    this.codVoo=codVoo;
-    this.origem=origem;
-    this.destino=destino;
-    this.assentosDisponiveis=assentosDisponiveis;
+class Carrinho {
+  constructor(itens, qtd, valorTotal) {
+    this.itens = itens;
+    this.qtd = qtd;
+    this.valorTotal = valorTotal;
   }
-  reservarAssento(){
-    if(this.assentosDisponiveis > 0){
-      this.assentosDisponiveis -= 1
-      console.log('Reservado com sucesso')
+
+  addItem(item) {
+
+    let contador = 0;
+
+    for(let itemCarrinho in this.itens) {
+      if(this.itens[itemCarrinho].id == item.id) {
+        this.itens[itemCarrinho].qtd += item.qtd;
+        contador = 1;
+      }
     }
 
+    if(contador == 0) {
+      this.itens.push(item);
+    }
+
+    this.qtd += item.qtd;
+    this.valorTotal += item.preco * item.qtd;
+
   }
-  consultarAssentosdisponiveis(){
-    console.log(`${this.assentosDisponiveis} assentos disponíveis`)
+
+  removeItem(item) {
+
+    for(let itemCarrinho in this.itens) {
+      if(this.itens[itemCarrinho].id == item.id) {
+        
+        let obj = this.itens[itemCarrinho];
+        let index = this.itens.findIndex(function(obj) { return obj.id == item.id});
+
+        this.qtd -= this.itens[itemCarrinho].qtd;
+        this.valorTotal -= this.itens[itemCarrinho].preco * this.itens[itemCarrinho].qtd;
+
+        this.itens.splice(index, 1);
+
+      }
+    }
   }
+
 }
 
-const voo=new Voo(1,'Juazema','Barcelona',15)
-console.log(voo)
-voo.reservarAssento()
-console.log(voo)
-voo.reservarAssento()
-console.log(voo)
-voo.consultarAssentosdisponiveis()
+let carrinho = new Carrinho([
+  {
+    id: 1,
+    nome: "Camisa",
+    qtd: 1,
+    preco: 20
+  },
+  {
+    id: 2,
+    nome: "Calça",
+    qtd: 2,
+    preco: 50
+  }
+], 3, 120);
+
+console.log(carrinho);
+
+carrinho.addItem({id:1, nome:"Camisa", qtd:2, preco: 20});
+
+console.log(carrinho);
+
+carrinho.addItem({id:3, nome:"Boné", qtd:1, preco: 15});
+
+console.log(carrinho);
+
+carrinho.removeItem({id: 1, nome: "Camisa", qtd: 1, preco: 20});
+
+console.log(carrinho);
+
+
---
+++ teste/script.js
@@ -0,0 +1,2 @@
+const reg = /^(?=.{3,16}$)[a-z0-9-_]/
+console.log(reg.test('dvd_121ffffffffff'))

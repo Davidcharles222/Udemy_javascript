ee3acdf4bafc6915aaa4c81773e28188d371b721
David <dvdcharles222@gmail.com>
2024-02-09 22:58:49 -0300

    Criando projeto jogo da velha

--- teste/script.js
+++ teste/script.js
@@ -1,18 +1,50 @@
-function* criadorId(){
-    let id = 0;
-    while(true){
-        yield id++;
-    };
-};
+let x = document.querySelector('.x');
+let o = document.querySelector('.o');
+let boxes = document.querySelectorAll('.box');
+let buttons = document.querySelectorAll('#buttons-container button');
+let messageContainer = document.querySelector('#message');
+let messageText = document.querySelector('#message p');
+let secondPlayer;
 
-let criaId = criadorId();
+// contador de jogadas 
+let player1 = 0;
+let player2 = 0;
 
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
-console.log(criaId.next().value)
+// adicionando o evento de click aos boxes
+for(let i = 0; i < boxes.length; i++){
+
+// quando alguem clica na caixa
+    boxes[i].addEventListener('click',function(){
+
+        let el = checkEl(player1,player2);
+
+        //verifica se já tem x ou o
+        if(this.childNodes.length == 0){
+
+            let cloneEl = el.cloneNode(true);
+
+            this.appendChild(cloneEl);
+
+            // computar jogada
+            if(player1 == player2){
+                player1++;
+            }else{
+                player2++;
+            }
+        }
+    });
+}
+
+
+//ve quem vai jogar
+function checkEl(player1,player2){
+    if(player1 == player2){
+        //x
+        el = x;
+    }else{
+        //o
+        el = o;
+    }
+
+    return el
+}
